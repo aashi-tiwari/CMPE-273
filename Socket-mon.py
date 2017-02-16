f38b31a4d891ab81a7b737135c1001eeed25a748
aashi22 <aashi.tiwari@sjsu.edu>
2017-02-15 16:47:06 -0800

    Add files via upload

---
+++ Socket-mon.py
@@ -0,0 +1,36 @@
+import socket  # This module provides socket functions
+import psutil  # This module provides information of running process
+
+lst = []       # Creation of Lists
+tcpConns = []
+id = []
+
+def main():  # It process Tcp connections as list of local address, remote address, Pid and statu
+    for con in psutil.net_connections(kind='tcp'): 
+        laddr = "%s@%s" % (con.laddr) 
+        raddr = ""
+        if con.raddr:
+            raddr = "%s:%s" % (con.raddr)
+            if raddr ==" ": # Skips all the record which will having no remote address
+             pass
+            else:
+                lst = [con.pid,laddr,raddr,con.status]
+                tcpConns.append(lst)
+                id.append(con.pid)  # appending all pid in one list
+    tcpConns.sort()
+    tracker = []
+    frequencyCount = []
+    for d in id:
+      if d not in tracker:
+          count = (id.count(d),d)  # counting number of connections and stores Pid and count of connection
+          frequencyCount.append(count)
+          sortFC = sorted(frequencyCount,reverse = True) 
+          tracker.append(d)
+    print( "PID","Local address", "Remote address", "Status")
+    for fc in sortFC:
+       for gettc in tcpConns: # matching the Pid from two list and print sorted list
+        if fc[1] == gettc[0]: 
+         print gettc
+             
+if __name__ == '__main__':
+ main()
